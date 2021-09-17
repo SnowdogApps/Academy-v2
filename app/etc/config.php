cae62e144bfc53e2469746ba085c7915f9083195
Mateusz Sobiech <maxis275@wp.pl>
2021-09-17 12:45:17 +0200

    Task 2 Create a new module named Academy_SmsSubscription. The module is to have a configuration that allows you to define whether the SMS subscription functionality is enabled or disabled.

--- app/etc/config.php
+++ app/etc/config.php
@@ -307,6 +307,7 @@ return [
         'Magento_Wishlist' => 1,
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
+        'Academy_SmsSubscription' => 1,
         'Amazon_Core' => 1,
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
