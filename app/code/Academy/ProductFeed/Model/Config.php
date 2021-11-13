bfce0048a6c1f8f9669a60462d82bd682e3e5b49
fureya89 <ewelinagoszczurna@gmail.com>
2021-11-13 21:33:32 +0100

    84293 Created module and configuration for ProductFeed module

---
+++ app/code/Academy/ProductFeed/Model/Config.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Academy\ProductFeed\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class Config
+{
+    private ScopeConfigInterface $scopeConfig;
+
+    public const FIELD_NAME_ENABLE = 'sms_subscription/general/enable';
+
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function isEnabled(): bool
+    {
+        return (bool)$this->scopeConfig->getValue(
+            self::FIELD_NAME_ENABLE,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+}
